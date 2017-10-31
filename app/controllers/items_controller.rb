0bd2e3fd19a8cf67340fde20a760debbc9be7f6d
Noora <nkmansou@qatar.cmu.edu>
2017-10-31 14:45:28 +0000

    Added daily weekly and monthly prices

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -73,6 +73,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :description, :user_id, :category_id)
+      params.require(:item).permit(:name, :description, :user_id, :category_id, :daily_price, :weekly_price, :monthly_price)
     end
 end
