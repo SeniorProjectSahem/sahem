16426d88868688b1cff7bc191fdae347b8aad3b8
Batoul hasnah <bmhasnah@andrew.cmu.edu>
2017-10-31 06:25:44 +0000

    home page and items page updated

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :users
   resources :locations
   
+ 
   root 'home#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
