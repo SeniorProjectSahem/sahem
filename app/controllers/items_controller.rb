4fe722465713cb9473810acdb712d2e06a0bd1d4
Layan <lya@qatar.cmu.edu>
2017-11-01 09:16:42 +0300

    layan5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -75,6 +75,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :description, :user, :category_id, :image)
+      params.require(:item).permit(:name, :description, :user, :category_id, :image,:daily_price, :monthly_price, :yearly_price)
     end
 end
